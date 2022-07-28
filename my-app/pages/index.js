f1b93f73028653a7ae33dda48fbc83f706ef6bf6
Sachin <heysachinkry@gmail.com>
2022-07-28 23:52:07 +0530

    hi

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -11,8 +11,16 @@ import {
 } from "../constants";
 
 export default function Home() {
+  const zero = BigNumber.from(0);
   const [walletConnected, setWalletConnected] = useState(false);
   const web3ModalRef = useRef();
+  const [balanceOfCryptoDevTokens, setBalanceOfCryptoDevTokens] = useState(zero);
+  // overall no of minted tokens
+  const [tokensMinted, setTokensMinted] = useState(zero);
+  // amount of tokens you want to mint
+  const [tokenAmount, setTokenAmount] = useState(zero);
+  const [tokensToBeClaimed, setTokensToBeClaimed] = useState(zero);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!walletConnected) {
@@ -22,10 +30,13 @@ export default function Home() {
         disableInjectedProvider: false
       });
       connectWallet();
+      getBalanceOfCryptoDevTokens();
+      getTotalTokensMinted();
+      getTokensToBeClaimed();
     }
-  }, []);
+    getTokensToBeClaimed();
+  }, [walletConnected]);
 
-  // 
 
   // connectWallet function 
   const connectWallet = async () => {
@@ -55,6 +66,205 @@ export default function Home() {
     return web3Provider;
   }
 
+  // withdraw function for owner to withdraw funds
+  const withdrawCoins = async () => {
+    try {
+      const signer = await getSignerOrProvider(true);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+      const txn = await tokenContract.withdraw();
+      setLoading(true);
+      await txn.wait();
+      setLoading(false);
+      await getOwner();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  //getowner
+  const getOwner = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider);
+      // call the owner function from the contract
+      const _owner = await tokenContract.owner();
+      // we get signer to extract address of currently connected Metamask account
+      const signer = await getProviderOrSigner(true);
+      // Get the address associated to signer which is connected to Metamask
+      const address = await signer.getAddress();
+      if (address.toLowerCase() === _owner.toLowerCase()) {
+        setIsOwner(true);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  // claim function for nft holders
+  const claimCryptoDevTokens = async () => {
+    try {
+      const signer = await getSignerOrProvider(true);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+      const txn = await tokenContract.claim();
+      setLoading(true);
+      await txn.wait();
+      setLoading(false);
+
+      window.alert("Succesfully claimed CryptoDev tokens");
+      await getBalanceOfCryptoDevTokens();
+      await getTotalTokensMinted();
+      await getTokensToBeClaimed();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // mint function for anyone to mint tokens
+  const mintCryptoDevTokens = async (amount) => {
+    try {
+      const signer = await getSignerOrProvider(true);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+      const value = 0.001 * amount;
+      const txn = await tokenContract.mint(amount, {
+        value: utils.parseEther(value.toString())
+      });
+      setLoading(true);
+      await txn.wait();
+      setLoading(false);
+
+      window.alert("Succesfully minted CryptoDev tokens")
+      await getBalanceOfCryptoDevTokens();
+      await getTotalTokensMinted();
+      await getTokensToBeClaimed();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // get no of tokens minted by a particular address
+  const getBalanceOfCryptoDevTokens = async () => {
+    try {
+      const provider = await getSignerOrProvider();
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
+      const signer = await getSignerOrProvider(true);
+      const address = await signer.getAddress();
+      const balance = await tokenContract.balanceOf(address);
+      setBalanceOfCryptoDevTokens(balance);
+    } catch (err) {
+      console.error(err);
+      setBalanceOfCryptoDevTokens(zero);
+    }
+  }
+
+  // get overall no of tokens minted
+  const getTotalTokensMinted = async () => {
+    try {
+      const provider = await getSignerOrProvider();
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
+      const _totalTokensMinted = await tokenContract.totalSupply();
+      setTokensMinted(_totalTokensMinted);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // get tokens to claimed by the nft holder
+  const getTokensToBeClaimed = async () => {
+    try {
+      const provider = await getSignerOrProvider();
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
+      );
+      const signer = await getSignerOrProvider(true);
+      const address = await signer.getAddress();
+      const balance = await nftContract.balanceOf(address);
+
+      if (balance === zero) {
+        setTokensToBeClaimed(zero);
+        console.log('nincompoop!');
+      } else {
+        var amount = 0;
+
+        for (var i; i < balance; i++) {
+          const tokenId = await nftContract.tokenOfOwnerByIndex(address, i);
+          const claimed = await tokenContract.tokenIdsClaimed(tokenId);
+
+          if (!claimed) {
+            amount++;
+          }
+          console.log("tokens claimed:", balance)
+        }
+        setTokensToBeClaimed(BigNumber.from(amount));
+      }
+    } catch (err) {
+      console.error(err);
+      setTokensToBeClaimed(zero);
+    }
+  }
+
+  const renderButton = () => {
+    if (loading) {
+      return (
+        <div>
+          <button className={styles.button}>loading...</button>
+        </div>
+      );
+    }
+
+    if (tokensToBeClaimed > 0) {
+      return (
+        <div>
+          <div className={styles.description}>
+            {tokensToBeClaimed * 10} CryptoDev tokens to be claimed
+          </div>
+          <button className={styles.button} onClick={claimCryptoDevTokens}>Claim Tokens</button>
+        </div>
+      )
+    }
+
+    return (
+      <div style={{ display: "flex-col" }}>
+        <input
+          type="number"
+          placeholder="Amount of tokens"
+          onChange={(e) => setTokenAmount(BigNumber.from(e.target.value))} />
+        <button
+          className={styles.button}
+          disabled={!(tokenAmount > 0)}
+          onClick={() => mintCryptoDevTokens(tokenAmount)}>Mint tokens</button>
+      </div>
+    )
+
+  }
 
   return (
     <div>
@@ -71,19 +281,30 @@ export default function Home() {
           </div>
 
           <div>
-            <div className={styles.description}>
-              {/* Format Ether helps us in converting a BigNumber to string */}
-              You have minted 0 Crypto
-              Dev Tokens
-            </div>
-            <div className={styles.description}>
-              {/* Format Ether helps us in converting a BigNumber to string */}
-              Overall 0/10000 have been minted!!!
-            </div>
+            {walletConnected ?
+              (
+                <div>
+                  <div className={styles.description}>
+                    {/* Format Ether helps us in converting a BigNumber to string */}
+                    You have minted {utils.formatEther(balanceOfCryptoDevTokens)} Crypto
+                    Dev Tokens
+                  </div>
+                  <div className={styles.description}>
+                    {/* Format Ether helps us in converting a BigNumber to string */}
+                    Overall {utils.formatEther(tokensMinted)}/10000 have been minted!!!
+                  </div>
+                  {renderButton()}
+                </div>
+              )
+              :
+              (
+                <button className={styles.button} onClick={connectWallet}>
+                  Connect your wallet
+                </button>
+
+              )
+            }
           </div>
-          <button className={styles.button} onClick={connectWallet}>
-            Connect your wallet
-          </button>
 
         </div>
         <div>
